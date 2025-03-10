59a1aa6e09216ef65ce27c704c97cfa36fee4647
SaracZ <saraczs@SaracZ-MacBook-Air.local>
2025-03-11 02:04:04 +0300

    Add basic App structure and fix initial rendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles/tailwind.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
-// tailwind.config.js
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: {
-            light: '#8884d8',
-            DEFAULT: '#6366f1',
-            dark: '#4f46e5',
-          },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#8884d8',
+          DEFAULT: '#6366f1',
+          dark: '#4f46e5',
         },
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
